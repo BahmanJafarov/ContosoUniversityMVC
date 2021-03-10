0c9331aa99a98df5d5b40d8439366ab5b694e50b
Bəhmən Cəfərov <30909255+Bahman-Jafarov@users.noreply.github.com>
2021-03-10 17:12:50 +0400

    .

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-﻿using ContosoUniversityMVC.Models;
+﻿using ContosoUniversityMVC.Data;
+using ContosoUniversityMVC.Models;
+using ContosoUniversityMVC.Models.SchoolViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -11,11 +14,24 @@ namespace ContosoUniversityMVC.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly SchoolContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(SchoolContext context)
         {
-            _logger = logger;
+            _context = context;
+        }
+
+        public async Task<IActionResult> About()
+        {
+            IQueryable<EnrollmentDateGroup> data = from student in _context.Students
+                                                   group student by student.EnrollmentDate into dateGroup
+                                                   select new EnrollmentDateGroup()
+                                                   {
+                                                       EnrollmentDate = dateGroup.Key,
+                                                       StudentCount = dateGroup.Count()
+                                                   };
+
+            return View(await data.AsNoTracking().ToListAsync());
         }
 
         public IActionResult Index()

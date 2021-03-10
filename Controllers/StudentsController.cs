121c6f14cd97a6fc9ac44dca3c6d3f574d2bc720
Bəhmən Cəfərov <30909255+Bahman-Jafarov@users.noreply.github.com>
2021-03-10 23:41:17 +0400

    .

--- Controllers/StudentsController.cs
+++ Controllers/StudentsController.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using ContosoUniversity.Models;
+using ContosoUniversityMVC.Models;
 using ContosoUniversityMVC.Data;
 
 namespace ContosoUniversityMVC.Controllers

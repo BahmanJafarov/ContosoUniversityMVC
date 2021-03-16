08307e231e890e1b13dc594bae2617ff4a56abf3
Bəhmən Cəfərov <30909255+Bahman-Jafarov@users.noreply.github.com>
2021-03-17 00:07:43 +0400

    .

--- Models/Department.cs
+++ Models/Department.cs
@@ -23,6 +23,9 @@ namespace ContosoUniversityMVC.Models
 
         public int? InstructorID { get; set; }
 
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
+
         public Instructor Administrator { get; set; }
         public ICollection<Course> Courses { get; set; }
     }
